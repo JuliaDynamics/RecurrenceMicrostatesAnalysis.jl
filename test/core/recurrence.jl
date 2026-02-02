5bb4fc49755a734de7aded6147f90ddc487c14be
Gabriel Ferreira <gabriel.vferreira@icloud.com>
2026-02-02 14:24:01 -0300

    fix recurrence test

--- test/core/recurrence.jl
+++ test/core/recurrence.jl
@@ -1,7 +1,7 @@
 using Test
 using RecurrenceMicrostatesAnalysis
 
-struct TestExpression <: RecurrenceExpression end
+struct TestExpression <: RecurrenceExpression{Float64, Euclidean} end
 
 @test_throws ArgumentError recurrence(TestExpression(), rand(100) |> StateSpaceSet, rand(100) |> StateSpaceSet, 1, 1)
 @test_throws ArgumentError recurrence(TestExpression(), rand(2, 100), rand(2, 100), (1, ), (1, ))
